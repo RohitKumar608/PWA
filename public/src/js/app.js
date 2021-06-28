0808b5a265c26025886e367648b2019cfe1130f2
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-06-28 11:04:26 +0530

    feat: added dynamic caching and delete caching on browser activate

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,23 +1,24 @@
-var deferredPrompt
+
+var deferredPrompt;
 
 if (!window.Promise) {
-  window.Promise = Promise
+  window.Promise = Promise;
 }
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function () {
-      console.log('Service worker registered!')
+      console.log('Service worker registered!');
     })
-    .catch(function (err) {
-      console.log(err)
-    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
-  console.log('beforeinstallprompt fired')
-  event.preventDefault()
-  deferredPrompt = event
-  return false
-})
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,42 @@
-const cache_urls = [
-  '/',
-  '/index.html',
-  '/src/js/app.js',
-  '/src/js/feed.js',
-  '/src/js/promise.js',
-  '/src/js/fetch.js',
-  '/src/js/material.min.js',
-  '/src/css/app.css',
-  '/src/css/feed.css',
-  '/src/images/main-image.jpg',
-  'https://fonts.googleapis.com/css?family=Roboto:400,700',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-]
+const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_DYNAMIC_NAME = 'dynamic-v2'
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event)
   event.waitUntil(
-    caches.open('static').then(function (cache) {
-      cache_urls.forEach(function (val) {
-        cache.add(val)
-      })
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log('[Service Worker] Precaching App Shell')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
     })
   )
 })
 
 self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ....', event)
+  event.waitUntil(
+    caches.keys().then((keyLists) => {
+      return Promise.all(
+        keyLists.map((key) => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
   return self.clients.claim()
 })
 
@@ -36,6 +47,13 @@ self.addEventListener('fetch', function (event) {
         return response
       } else {
         return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone())
+              return res
+            })
+          })
+          .catch((err) => {})
       }
     })
   )
