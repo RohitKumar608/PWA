1243cc36947338ef6df5a0ee5322aa697e865c75
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-06-28 09:26:59 +0530

    feat: cached multiple files

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,42 @@
+const cache_urls = [
+  '/',
+  '/index.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+]
 self.addEventListener('install', function (event) {
-  console.log('Installing service worker ....', event)
+  console.log('[Service Worker] Installing Service Worker ...', event)
+  event.waitUntil(
+    caches.open('static').then(function (cache) {
+      cache_urls.forEach(function (val) {
+        cache.add(val)
+      })
+    })
+  )
 })
 
 self.addEventListener('activate', function (event) {
-  console.log('Activating service worker ....', event)
+  console.log('[Service Worker] Activating Service Worker ....', event)
   return self.clients.claim()
 })
 
 self.addEventListener('fetch', function (event) {
-  console.log('[Service Worker] Fetching something ....', event)
-  event.respondWith(fetch(event.request))
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response
+      } else {
+        return fetch(event.request)
+      }
+    })
+  )
 })
